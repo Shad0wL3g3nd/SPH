a8cf9bbeda695212f814f723690d7d10271f9ddd
Shadow <178309739+Shad0wL3g3nd@users.noreply.github.com>
2024-11-08 10:41:12 -0500

    Update BrowserPopUp.js

--- Testing/BrowserPopUp/BrowserPopUp.js
+++ Testing/BrowserPopUp/BrowserPopUp.js
@@ -1,2 +1,2 @@
 var x = window.open('') // open a blank tab;
-x.document.write('<iframe src="https://bing.com" allowfullscreen="true" allowpaymentrequest="true" hight="300" width="600" loading="eager" name="SPH Secure" referrerpolicy="origin" sandbox="allow-scripts" ></iframe>');
+x.document.write('<iframe src="https://duckduckgo.com" allowfullscreen="true" allowpaymentrequest="true" hight="300" width="600" loading="eager" name="SPH Secure" referrerpolicy="origin" sandbox="allow-scripts" ></iframe>');
